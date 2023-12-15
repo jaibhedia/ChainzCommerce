4c044c71f6a0a8ad7612ae4138f48c5fe0a2fab4
Nandish <nandish1729@gmail.com>
2023-12-15 18:27:54 +0530

    made responsive with animation

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,17 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import nftImg from "./assets/nfteth.jpeg";
 import item1 from "./assets/item1.jpeg";
 import item2 from "./assets/etheth.jpeg";
 import item3 from "./assets/it3.jpeg";
 import item4 from "./assets/it4.jpeg";
-import image from "./assets/img1.jpeg";
+import logo from "./assets/logo.jpeg";
+import additionalItem from "./assets/item1.jpeg";
+import additionalItem2 from "./assets/it4.jpeg";
+import additionalItem3 from "./assets/it3.jpeg";
 import { Button, Nav } from "react-bootstrap";
 import { GrInstagram, GrTwitch, GrTwitter } from "react-icons/gr";
 import { FaFacebook } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "./styles/style.css";
 
 const Home = ({ marketplace, nft, account, web3Handler }) => {
   const [loading, setLoading] = useState(true);
+  const sectionRef = useRef(null);
   const [items, setItems] = useState([]);
   const loadMarketplaceItems = async () => {
     // Load all unsold items
@@ -48,77 +53,102 @@ const Home = ({ marketplace, nft, account, web3Handler }) => {
     ).wait();
     loadMarketplaceItems();
   };
-
+  useEffect(() => {
+    const section = sectionRef.current;
+    section.classList.add("fade-in");
+  }, []);
   useEffect(() => {
     loadMarketplaceItems();
   }, []);
   return (
     <>
-      <div className="px-8 flex justify-center items-center relative">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:px-8 lg:px-20">
-          <div className="flex flex-col justify-center text-left -mt-10">
-            <h2 className="text-4xl md:text-5xl lg:text-6xl  text-white font-bold mb-4">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-green-400">
-                Data{" "}
-              </span>
-              is your
-              <br />
-              inexhaustible
-              <br />
-              asset
+      <section ref={sectionRef} className="py-6" id="null">
+        <div className="mx-auto mt-10 max-w-7xl px-6 lg:px-8">
+          <div className=" mx-auto mt-10 grid grid-cols-1 pt-10 max-w-2xl lg:max-w-full lg:mx-0 md:grid-cols-2 gap-x-8 gap-y-16 sm:mt-16 space-x-4 ">
+            <div className="md:order-1 sm:px-4">
+              <div className="text-left md:text-left">
+                <p className="text-3xl text-white leading-relaxed sm:text-left">
+                  <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-green-400">
+                    Data{" "}
+                  </span>
+                  is your inexhaustible asset
+                </p>
+                <p className="text-sm md:text-lg text-gray-600 mb-4">
+                  Confirm data to trade, earn, and win crypto assets in a
+                  decentralized privacy-preserving network
+                </p>
+                <div className="bg-blue-500 hover:scale-110 hover:transition-transform font-bold bg-gradient-to-r from-blue-700 to-green-400 text-white py-2 h-11 text-center w-full sm:w-3/4 md:w-1/2 lg:w-1/3 px-4 rounded cursor-pointer hover:bg-blue-600">
+                  <div>
+                    {account ? (
+                      <a
+                        href={`https://etherscan.io/address/${account}`}
+                        target="_blank"
+                      >
+                        <p className="text-white">Connected</p>
+                      </a>
+                    ) : (
+                      <Button onClick={web3Handler} variant="outline-light">
+                        Connect Wallet
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="mt-8 text-left">
+                <h3 className="text-2xl text-white font-bold mb-4">
+                  Why Choose Our NFT Marketplace?
+                </h3>
+                <ul className="text-gray-400">
+                  <li className="flex items-center mb-2">
+                    <span className="mr-2">&#10003;</span> Decentralized and
+                    Secure Transactions
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <span className="mr-2">&#10003;</span> Wide Range of NFT
+                    Options to Explore
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <span className="mr-2">&#10003;</span> Earn Rewards for
+                    Trading Your NFTs
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="md:order-2 lg:w-full lg:h-2/3 sm:w-1/2  sm:mx-auto">
+              <img
+                src={nftImg}
+                alt="about"
+                className="object-cover w-full h-full rounded-md shadow-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section ref={sectionRef} className="my-9">
+        <div className="relative px-8 rounded-lg shadow-lg">
+          <div className="max-w-2xl mx-auto fade-in-section">
+            <h2 className="text-3xl font-bold mb-4 text-white">
+              Create your NFT List Which makes your data Worth
             </h2>
-            <p className="text-lg text-gray-600 mb-8">
-              Confirm data to trade, earn, and win crypto assets in a
-              decentralized privacy-preserving network
+            <p className="text-gray-600 mb-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
-            <div className="bg-blue-500 hover:scale-110 hover:transition-transform font-bold bg-gradient-to-r from-blue-700 to-green-400 text-white py-2  h-11 text-center w-1/2 px-4 rounded cursor-pointer hover:bg-blue-600">
-              <div>
-                {account ? (
-                  <a
-                    href={`https://etherscan.io/address/${account}`}
-                    target="_blank"
-                  >
-                    <p className=" text-white">Connected</p>
-                  </a>
-                ) : (
-                  <Button onClick={web3Handler} variant="outline-light">
-                    Connect Wallet
-                  </Button>
-                )}
+            <div className="flex flex-col items-center justify-center">
+              <div className="bg-none border-3 border-blue-700 text-white py-2 w-full md:w-2/3 lg:w-1/2 px-6 rounded-none cursor-pointer hover:bg-blue-700">
+                <Nav.Link
+                  as={Link}
+                  to="/create"
+                  className="text-center text-white"
+                >
+                  Create your NFT
+                </Nav.Link>
               </div>
             </div>
           </div>
-          <div className="hidden md:block  h-4/6  mt-14 relative">
-            <img
-              src={nftImg}
-              alt="NFT"
-              className="rounded-lg h-full w-full object-contain"
-            />
-          </div>
         </div>
-      </div>
-      <div className="relative py-4 -mt-10 px-8 rounded-lg shadow-lg">
-        <div className="max-w-2xl mx-auto">
-          <h2 className="text-3xl font-bold mb-4 text-white">
-            Create your NFT List Which makes your data Worth
-          </h2>
-          <p className="text-gray-600 mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <div className="flex items-center justify-center h-full">
-            <div className="bg-none border-3 border-blue-700 text-white py-2 w-2/6 px-6 rounded-none cursor-pointer hover:bg-blue-700">
-              <Nav.Link
-                as={Link}
-                to="/create"
-                className="text-center text-white"
-              >
-                Create your NFT
-              </Nav.Link>
-            </div>
-          </div>
-        </div>
-      </div>
+      </section>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-4">
         <div className="bg-none border-3 border-gray-500 p-4 rounded-md relative hover:scale-105 hover:transition-transform cursor-pointer">
@@ -160,45 +190,67 @@ const Home = ({ marketplace, nft, account, web3Handler }) => {
         </div>
       </div>
 
-      <div className="relative w-full h-1/2 overflow-hidden">
-        <img
-          src={image}
-          className="w-full h-full object-cover opacity-60  sm:min-h-screen"
-          alt="About"
-        />
-        <div className="absolute inset-0 bg-black opacity-70"></div>
-        <div className="absolute inset-0 flex items-center justify-center text-white">
-          <div className="text-left p-1  w-3/4">
-            <h1 className="text-4xl  md:text-4xl font-sembold  tracking-wider mb-6">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h1>
-            <div className="mb-8">
-              <p className="text-lg">See your NFTs List</p>
-            </div>
-            {/* Add your CTA button or link here */}
-            <Nav.Link
-              as={Link}
-              to="/my-listed-items"
-              className="bg-none border-3 border-blue-500 w-1/6 font-semibold text-white py-2 px-6 rounded-none hover:bg-blue-500 transition duration-300"
-            >
-              Explore NFTs
-            </Nav.Link>
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-4">
+        <div className="bg-none border-3 border-gray-500 p-4 rounded-md cursor-pointer">
+          <img src={additionalItem} alt="additionalItem" />
+          <p className="text-gray-300">
+            Exclusive Collections <br />
+            <span className="font-semibold text-white text-xl tracking-wide">
+              Discover limited edition NFT collections.
+            </span>
+          </p>
+        </div>
+
+        <div className="bg-none border-3 border-gray-500 p-4 rounded-md cursor-pointer">
+          <img src={additionalItem2} alt="additionalItem2" />
+          <p className="text-gray-300">
+            Community Engagement <br />
+            <span className="font-semibold text-white text-xl tracking-wide">
+              Connect with other NFT enthusiasts and creators.
+            </span>
+          </p>
+        </div>
+
+        <div className="bg-none border-3 border-gray-500 p-4 rounded-md cursor-pointer">
+          <img src={additionalItem3} alt="additionalItem3" />
+          <p className="text-gray-300">
+            Governance Features <br />
+            <span className="font-semibold text-white text-xl tracking-wide">
+              Participate in platform decisions with governance.
+            </span>
+          </p>
         </div>
       </div>
+
       <div className="bg-none text-white p-4 flex flex-row justify-between items-center">
-        <div className="text-2xl font-bold">NFT Marketplace</div>
-        <div className="space-x-4">
-          <a href="/" className=" hover:text-white hover:scale-110">
+        <div className="flex-shrink-0">
+          <img src={logo} className="w-20 h-20 rounded-full" alt="logo" />
+        </div>
+
+        <div className=" flex flex-row space-x-4">
+          <Nav.Link
+            as={Link}
+            to="/"
+            className=" hover:text-white text-white  hover:scale-110"
+          >
             Home
-          </a>
-          <a href="about" className=" hover:text-white hover:scale-110">
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/"
+            href="about"
+            className=" hover:text-white text-white  hover:scale-110"
+          >
             About
-          </a>
-          <a href="help" className=" hover:text-white hover:scale-110">
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/"
+            href="help"
+            className=" hover:text-white text-white  hover:scale-110"
+          >
             Help
-          </a>
+          </Nav.Link>
         </div>
         <div className="flex flex-row space-x-4 ">
           <a href="#" className="text-xl hover:text-white hover:scale-110">
